228fa3bb75a6605ac76c0196e8bb3e28ddc3bd58
Tsigereda <tsigeredasfw@gmail.com>
2017-05-25 22:53:26 +0200

    add to the education object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,18 +1,18 @@
 
 
 var bio = {
-    "name" : "Tsigereda A. Demisse",
-    "role" : "Senior consultant",
-    "contacts" :{
-        "mobile" : "94152442",
-        "email" : "tsigeredasfw@gmail.com",
-        "github" : "tsigereda",
-        "location" : "Stavanger"
+    name : "Tsigereda A. Demisse",
+    role : "Senior consultant",
+    contacts :{
+        mobile : "94152442",
+        email : "tsigeredasfw@gmail.com",
+        github : "tsigereda",
+        location : "Stavanger"
     },
-    "welcomeMessage" : "Hi, welcome and thanks for the visit. Take contact, i am friendly or so i think :)",
-    "skills" : ["SharePoint",".Net", "JS", "MVC"],
-    "biopic":"images/me.jpg",
-    "display" : function(){
+    welcomeMessage : "Hi, welcome and thanks for the visit. Take contact, i am friendly or so i think :)",
+    skills : ["SharePoint",".Net", "JS", "MVC"],
+    biopic:"images/me.jpg",
+    display : function(){
 
        
         var formattedName= HTMLheaderName.replace("%data%",this.name);
@@ -40,57 +40,65 @@ var bio = {
 bio.display();
 
 var education = {
-    "schools" : [{
-        "name" : "Haramaya University/ Alemaya University",
-        "City" : "Harrar",
-        "degree" : "Bachelors degree in Computer Science and IT",
-        "dates" : 2006
+    school : [{
+        name : "Haramaya University/ Alemaya University",
+        location : "Harrar",
+        degree : "Bachelors degree in Computer Science and IT",
+        majors : ["Computer science","Software development", "Numerical methods"],
+        dates : "2003 - 2006"
     },{
-        "name" : "Oslo University college",
-        "City" : "Oslo",
-        "degree" : " Joint Masters degree in Digital Libraries learning",
-        "dates" : 2009
+        name : "Oslo University college",
+        location : "Oslo",
+        degree : " Joint Masters degree in Digital Libraries learning",
+        majors : ["Information Science","Knowledge managment", "Collaboration and information managment"],
+        dates : "2007 - 2009"
     }],
-    "OnlineCourses":[
+    OnlineCourses:[
     {
-        "name" : "C# learning path",
-        "School" : "Pluralsight",
-        "dates":2016,
-        "url":"https://www.pluralsight.com/paths/csharp"        
+        title : "C# learning path",
+        School : "Pluralsight",
+        dates:"2016",
+        url:"https://www.pluralsight.com/paths/csharp"        
     },
     {
-        "name" : "Front-end web developer Nanodegree",
-        "School" : "Udacity",
-        "dates":2017,
-        "url":"https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"        
+        name : "Front-end web developer Nanodegree",
+        School : "Udacity",
+        dates:"2017",
+        url:"https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"        
+    }],
+    display : function (){
+        $("#education").append(HTMLschoolStart);
+       this.school.forEach(function(element) {
+            $()
+        }, this);
     }
-    ]
 }
 
 var work={
-    "jobs" :[{
-        "employer" :"Bahirdar University",
-        "title": "Graduate assistant",
-       "dates":"July 2006 - July 2017",
-       "location" : "Bahirdar, Ethiopia",
-       "description":"Thought introduction to computer science and programing courses for computer science students. Project adviser and assistant for more advanced subjects for the last year students" 
+    jobs :[{
+        employer :"Bahirdar University",
+        title: "Graduate assistant",
+       dates:"July 2006 - July 2017",
+       location : "Bahirdar, Ethiopia",
+       description:"Thought introduction to computer science and programing courses for computer science students. Project adviser and assistant for more advanced subjects for the last year students" 
             },
-            {"employer" :"CERN",
-            "title": "Intern",
-            "dates":"September 2008 - December 2008",
-            "location" : "Genev, Switzerland",
-            "description":"Python developer in the IT department developing and maintaining the opensource digital library software."},
+            {employer :"CERN",
+            title: "Intern",
+            dates:"September 2008 - December 2008",
+            location : "Genev, Switzerland",
+            description:"Python developer in the IT department developing and maintaining the opensource digital library software."},
             
-            {"employer" :"Statoil ASA",
-            "title": "Solutions Architect",
-            "dates":"July 2010 - June 2014",
-             "location" : "Stavanger, Norway",
-            "description":"Worked mainly with SharePoint and applications built ontop of it. I worked  in the department maintaining and developing the companies collaboration and document managment system"
+            {employer :"Statoil ASA",
+            title: "Solutions Architect",
+            dates:"July 2010 - June 2014",
+            location : "Stavanger, Norway",
+            description:"Worked mainly with SharePoint and applications built ontop of it. I worked  in the department maintaining and developing the companies collaboration and document managment system"
         },
-        {"employer" :"Capgemini Norway",
-        "title": "Senior consultant",
-       "dates":"June 2014 - Present",
-       "description":" Belogs to a software engineering department and i work with a team that maintains and develops .Net applications for customers in the Oil and Gas industry. "
+        {employer :"Capgemini Norway",
+         title: "Senior consultant",
+         dates:"June 2014 - Present",
+         location:"Stavanger, Norway",
+          description:" Belogs to a software engineering department and i work with a team that maintains and develops .Net applications for customers in the Oil and Gas industry. "
         }]
 }
 
